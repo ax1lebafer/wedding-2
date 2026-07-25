a0be2e2adcdccc89ee56c21b35765127839e4415
ax1lebafer <raul8917@gmail.com>
2026-07-26 01:23:36 +0400

    Add reveal animations for venue elements, enhancing visibility and user interaction

--- src/sections/Venue/Venue.tsx
+++ src/sections/Venue/Venue.tsx
@@ -1,4 +1,5 @@
 import decor from '../../assets/decor_3.svg'
+import { useInView } from '../../hooks/useInView'
 import { wedding } from '../../data/wedding'
 import './Venue.scss'
 
@@ -6,20 +7,57 @@ const { lon, lat } = wedding.venue.coords
 
 const mapSrc = `https://yandex.ru/map-widget/v1/?ll=${lon}%2C${lat}&z=16.5&pt=${lon}%2C${lat}%2Cpm2rdm&lang=ru_RU`
 
-const mapHref = `https://yandex.ru/maps/?ll=${lon}%2C${lat}&z=17&pt=${lon}%2C${lat}%2Cpm2rdm`
+const mapHref = `https://yandex.ru/maps/?rtext=~${lat}%2C${lon}&rtt=auto`
 
 export function Venue() {
+  const { ref: headingRef, isInView: headingVisible } =
+    useInView<HTMLHeadingElement>({ threshold: 0.4 })
+  const { ref: addressRef, isInView: addressVisible } =
+    useInView<HTMLParagraphElement>({ threshold: 0.4 })
+  const { ref: mapRef, isInView: mapVisible } = useInView<HTMLDivElement>({
+    threshold: 0.2,
+  })
+
   return (
     <section className="venue">
-      <h2 className="venue__heading">{wedding.venue.title}</h2>
+      <h2
+        ref={headingRef}
+        className={[
+          'venue__heading',
+          'venue__reveal',
+          headingVisible ? 'venue__reveal--visible' : '',
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      >
+        {wedding.venue.title}
+      </h2>
 
-      <p className="venue__address">
+      <p
+        ref={addressRef}
+        className={[
+          'venue__address',
+          'venue__reveal',
+          addressVisible ? 'venue__reveal--visible' : '',
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      >
         {wedding.venue.lines.map((line) => (
           <span key={line}>{line}</span>
         ))}
       </p>
 
-      <div className="venue__map-wrap">
+      <div
+        ref={mapRef}
+        className={[
+          'venue__map-wrap',
+          'venue__reveal',
+          mapVisible ? 'venue__reveal--visible' : '',
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      >
         <iframe
           className="venue__map"
           src={mapSrc}
